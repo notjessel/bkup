84e8a9a8057f9ad509826b02b7efc088ae76ee39
jesselp666 <farmfiddlin@gmail.com>
2019-11-10 20:53:25 -0500

    stuffs

---
+++ .config/dwmblocks/blocks.h
@@ -0,0 +1,14 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		                     /*Update Interval*/	/*Update Signal*/
+    {"", "~/.config/dwmblocks/statusbar/pkgupdates",				 10,	1},
+    {"", "~/.config/dwmblocks/statusbar/clock",						 1,		1},
+    {"", "~/.config/dwmblocks/statusbar/pulse",						 5,		1},
+    {"", "~/.config/dwmblocks/statusbar/mpd",						 1,		1},
+    {"", "~/.config/dwmblocks/statusbar/date",					     60,	1},
+    {"", "~/.config/dwmblocks/statusbar/backlight",                  5,     1},
+    {"", "~/.config/dwmblocks/statusbar/battery",					 10,	1},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = '|';
