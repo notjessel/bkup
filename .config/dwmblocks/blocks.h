1c6c5683d624f1e07239fdb182e36c5d6ab38838
jesselp666 <farmfiddlin@gmail.com>
2019-12-21 00:00:07 -0500

    stuffs

--- .config/dwmblocks/blocks.h
+++ .config/dwmblocks/blocks.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		                     /*Update Interval*/	/*Update Signal*/
+	/*Icon*/	/*Command*/		                        /*Update Interval*/	/*Update Signal*/
+    {"", "~/.config/dwmblocks/statusbar/pkgupdates",				 10,	1},
     {"", "~/.config/dwmblocks/statusbar/clock",						 1,		1},
     {"", "~/.config/dwmblocks/statusbar/pulse",						 5,		1},
     {"", "~/.config/dwmblocks/statusbar/mpd",						 1,		1},
